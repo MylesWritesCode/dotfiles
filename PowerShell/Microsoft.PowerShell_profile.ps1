7aeab240c0015b9e24099538fd6866ca3c5a6682
Myles Berueda <myles@themapletree.io>
2023-04-11 13:34:02 -1000

    updates

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,2 @@
-# Place writescode.json into ~\Documents\Powershell
+oh-my-posh init pwsh --config ~\windows-dotfiles\oh-my-posh\tomorrow-night-bright.json | Invoke-Expression
 
-oh-my-posh init pwsh --config ~\Documents\windows-dotfiles\oh-my-posh\tomorrow-night-bright.json | Invoke-Expression

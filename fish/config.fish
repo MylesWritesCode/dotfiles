4e5bdbdc80f591cbb76ade95ea89efa554632d6f
Myles Berueda <myles@themapletree.io>
2023-07-24 17:16:07 -1000

    minor updates to starship theme

--- fish/config.fish
+++ fish/config.fish
@@ -5,10 +5,11 @@ end
 # oh-my-posh init fish --config ~/.config/oh-my-posh/tomorrow-night-bright.json | source
 
 # pnpm
-set -gx PNPM_HOME /Users/melllbby/Library/pnpm
+set -gx PNPM_HOME "/home/myles/.pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
 
 starship init fish | source
+enable_transience
---
+++ fish/functions/starship_transient_prompt_func.fish
@@ -0,0 +1,7 @@
+function starship_transient_prompt_func
+  starship module character
+end
+
+function starship_transient_rprompt_func
+  starship module time
+end

04e34cae669de93cbd203a2109a11436934e2edd
Myles Berueda <myles@themapletree.io>
2023-07-25 17:35:12 -1000

    removes more fish variables

--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# oh-my-posh init fish --config ~/.config/oh-my-posh/tomorrow-night-bright.json | source
-
 # pnpm
-set -gx PNPM_HOME "/home/myles/.pnpm"
+set -gx PNPM_HOME "/Users/melllbby/Library/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
@@ -13,3 +11,5 @@ end
 
 starship init fish | source
 enable_transience
+
+# oh-my-posh init fish --config ~/.config/oh-my-posh/tomorrow-night-bright.json | source
--- fish/functions/starship_transient_prompt_func.fish
+++ fish/functions/starship_transient_prompt_func.fish
@@ -1,7 +1,7 @@
 function starship_transient_prompt_func
-  starship module character
+    starship module character
 end
 
 function starship_transient_rprompt_func
-  starship module time
+    starship module time
 end

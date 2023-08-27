24912f7964f60ef02bef7e752ab26fd1e92f52ab
Myles Berueda <myles@themapletree.io>
2023-08-26 21:05:40 -1000

    testing nix macos

---
+++ nix/README.md
@@ -0,0 +1,4 @@
+## Instructions
+1. Install [Nix](https://nixos.org/download)
+2. Run `nix run . switch` to install `home-manager`
+
--- nix/flake.nix
+++ nix/flake.nix
@@ -10,12 +10,13 @@
   outputs = inputs: {
     defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
     defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
+    defaultPackage.aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
 
     homeConfigurations = {
       # Create a file named username.nix and and run echo $USER > username.nix
       "melllbby" = inputs.home-manager.lib.homeManagerConfiguration {
-        system = "x86_64-darwin";
-        homeDirectory = "/home/melllbby";
+        system = "aarch64-darwin";
+        homeDirectory = "/Users/melllbby";
         username = "melllbby";
         configuration.imports = [ ./home.nix ];
       };

706268f6945b8e96cf70344c266eaa2ab3a93726
Myles Berueda <myles@themapletree.io>
2023-08-26 21:56:48 -1000

    testing nix lockfiles

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,21 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs: {
+  outputs = { nixpkgs, home-manager, ... }: {
     defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
     defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
     defaultPackage.aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
 
     homeConfigurations = {
       # Create a file named username.nix and and run echo $USER > username.nix
-      "melllbby" = inputs.home-manager.lib.homeManagerConfiguration {
+      "melllbby" = home-manager.lib.homeManagerConfiguration {
         system = "aarch64-darwin";
-        homeDirectory = "/Users/melllbby";
         username = "melllbby";
+        homeDirectory = "/Users/melllbby";
         configuration.imports = [ ./home.nix ];
       };
     };

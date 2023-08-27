b0c8ac62bbb63fef7b766887baa5d9ea9e28f3ca
Myles Berueda <myles@themapletree.io>
2023-08-26 20:42:05 -1000

    adds home-manager, testing flakes

--- home-manager/configuration.nix
+++
@@ -1,8 +0,0 @@
-let
-  username = import ./username.nix
-in
-{
-  home.username = username;
-  home.homeDirectory = "/home/${username}"
-}
-
---
+++ nix/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Home Manager flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs: {
+    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+    defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
+
+    homeConfigurations = {
+      # Create a file named username.nix and and run echo $USER > username.nix
+      "melllbby" = inputs.home-manager.lib.homeManagerConfiguration {
+        system = "x86_64-darwin";
+        homeDirectory = "/home/melllbby";
+        username = "melllbby";
+        configuration.imports = [ ./home.nix ];
+      };
+    };
+  };
+}

446cd9556f2e9a45d11a771dfa2d56c8fe8bfc92
Myles Berueda <myles@themapletree.io>
2023-08-27 19:13:45 -1000

    testing flake.nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,7 +1,15 @@
 {
-  "description": "Flake home-manager configuration";
+  description = "NixOS config"
+
   inputs = {
-    nixpkgs.url = "github:dustinlyons/nixpkgs/master";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+
   };
 }

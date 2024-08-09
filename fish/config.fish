c54d464b23fd95fb17dfcb0af9eeed013d4d3470
Myles Berueda <myles@themapletree.io>
2024-08-09 12:01:10 -0700

    feat(fish): adds starship back into the mix

--- fish/config.fish
+++ fish/config.fish
@@ -7,15 +7,6 @@ set -gx PNPM_HOME "/home/myles/.pnpm/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
-
-starship init fish | source
-enable_transience
-
-# pnpm
-set -gx PNPM_HOME "/home/myles/.pnpm/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
-end
 # pnpm end
 
 # pyenv
@@ -25,19 +16,4 @@ pyenv init - | source
 # zoxide, a better cd
 zoxide init --cmd cd fish | source
 
-# ld_library_path
-# @note looks like if this is set and we're also using ldconfig, some programs will fail to run
-# set -gx LD_LIBRARY_PATH /usr/local/cuda-12.4/lib64:/usr/local/lib/x86_64-linux-gnu:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /home/myles/miniconda3/bin/conda
-    eval /home/myles/miniconda3/bin/conda "shell.fish" hook $argv | source
-else
-    if test -f "/home/myles/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/home/myles/miniconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH /home/myles/miniconda3/bin $PATH
-    end
-end
-# <<< conda initialize <<<
+starship init fish | source

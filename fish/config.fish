29e1f4574f78e3189276ec121d17aa2bb2155775
Myles Berueda <myles@themapletree.io>
2024-09-01 18:36:34 -0700

    feat(fish): updates pnpm

--- fish/config.fish
+++ fish/config.fish
@@ -17,3 +17,10 @@ pyenv init - | source
 zoxide init --cmd cd fish | source
 
 starship init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/myles/.pnpm/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end

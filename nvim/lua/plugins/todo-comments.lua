e2de3432d54bac1a55781d47b88eebf902a8cea5
Myles Berueda <myles@themapletree.io>
2024-09-24 23:54:49 -0700

    feat(nvim): adds multicursor

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -7,16 +7,16 @@ return {
     -- keywords recognized as todo comments
     keywords = {
       FIX = {
-        icon = " ", -- icon used for the sign, and in search results
+        icon = "🐛", -- icon used for the sign, and in search results
         color = "error", -- can be a hex color, or a named color (see below)
         alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
         -- signs = false, -- configure signs for some keywords individually
       },
-      TODO = { icon = " ", color = "info" },
-      HACK = { icon = " ", color = "warning" },
-      WARN = { icon = " ", color = "warning", alt = { "XXX" } },
-      PERF = { icon = " ", alt = { "OPTIM", "OPTIMIZE" } },
-      NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+      TODO = { icon = "✅", color = "info" },
+      HACK = { icon = "🔥", color = "warning", alt = { "DELETEME" } },
+      WARN = { icon = "🚧", color = "warning", alt = { "XXX" } },
+      PERF = { icon = "🚀", alt = { "OPTIM", "OPTIMIZE", "SAFETY" } },
+      NOTE = { icon = "📝", color = "hint", alt = { "INFO" } },
       TEST = { icon = "⏲ ", color = "test", alt = { "PASS", "FAIL" } },
     },
     gui_style = {
@@ -33,9 +33,9 @@ return {
       multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
       multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
       before = "", -- "fg" or "bg" or empty
-      keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+      keyword = "fg", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
       after = "fg", -- "fg" or "bg" or empty
-      pattern = [[.*<(KEYWORDS):\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
+      pattern = [[.*<(KEYWORDS)\s\(.*\):]], -- pattern or table of patterns, used for highlighting (vim regex)
       comments_only = true, -- uses treesitter to match keywords in comments only
       max_line_len = 400, -- ignore lines longer than this
       exclude = {}, -- list of file types to exclude highlighting
@@ -61,7 +61,7 @@ return {
       },
       -- regex that will be used to match keywords.
       -- don't replace the (KEYWORDS) placeholder
-      pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+      pattern = [[\b(KEYWORDS)\s\(.*\):]], -- ripgrep regex
       -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
     },
   },

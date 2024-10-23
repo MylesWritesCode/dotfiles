4816613ae532dcb498d80f9fff5004ca6c1e6009
Myles Berueda <myles@themapletree.io>
2024-10-23 04:10:48 -0700

    adds keybind for diffview

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,3 +1,17 @@
 return {
   "sindrets/diffview.nvim",
+  keys = {
+    {
+      "<leader>gdo",
+      function()
+        local input = vim.fn.input("Commits: ")
+        if input ~= "" then
+          vim.cmd("DiffviewOpen " .. input)
+        else
+          vim.cmd("DiffviewOpen")
+        end
+      end,
+      desc = "🔭 Open Diffview",
+    },
+  },
 }

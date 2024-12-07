047e4502016b6705f0311e9323fe5643ea209d30
Myles Berueda <myles@themapletree.io>
2024-12-07 10:41:28 -0800

    fix(nvim): adds workaround for server cancelled request

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,5 +18,16 @@ if vim.fn.executable("nu") == 1 then
   vim.opt.shellxquote = ""
 end
 
+-- rust-analyzer workaround pre v0.11 neovim
+for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
+  local default_diagnostic_handler = vim.lsp.handlers[method]
+  vim.lsp.handlers[method] = function(err, result, context, config)
+    if err ~= nil and err.code == -32802 then
+      return
+    end
+    return default_diagnostic_handler(err, result, context, config)
+  end
+end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")

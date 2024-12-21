31aa4aae023538d151017976a77885c8976add56
Myles Berueda <myles@themapletree.io>
2024-12-20 16:10:15 -1000

    chore(nvim): cleanup default configs

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,6 +3,7 @@
 -- Add any additional options here
 -- vim.g.lazyvim_picker = "telescope"
 
+vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
 vim.g.lazyvim_blink_main = true -- Remove this when blink is fixed
 vim.g.snacks_animate = false
 

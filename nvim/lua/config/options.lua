95ec6b6b7e5f8284a7e0fa29f930864820575fe5
Myles Berueda <myles@themapletree.io>
2025-02-03 22:43:51 -1000

    fix(nvim): adds notes for cross-os clipboard

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -9,3 +9,32 @@ vim.g.snacks_animate = true
 local opt = vim.opt
 
 opt.colorcolumn = "80,120"
+
+vim.keymap.set("n", "<leader>y", function()
+  vim.fn.setreg(vim.v.register, vim.fn.expand("%:p"))
+end, { desc = "Copy filename+line to clipboard" })
+
+-- This is the old way to do it. Instead, just install wl-clipboard for
+-- cross-os copy/paste with `sudo apt install wl-clipboard`
+-- Use the right clipboard when on WSL
+-- local function is_wsl()
+--   local wsl_check = os.getenv("WSL_DISTRO_NAME") ~= nil
+--   return wsl_check
+-- end
+--
+-- if is_wsl() then
+--   vim.g.clipboard = {
+--
+--     name = "WSL Yank",
+--     copy = {
+--       ["+"] = "clip.exe",
+--       ["*"] = "clip.exe",
+--     },
+--
+--     paste = {
+--       ["+"] = "win32yank.exe -o --lf",
+--       ["*"] = "win32yank.exe -o --lf",
+--     },
+--     cache_enabled = 1,
+--   }
+-- end

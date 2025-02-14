ec2ca220c17c3d63086850341dd40d04ee20be4d
Myles Berueda <myles@themapletree.io>
2025-02-13 17:37:07 -1000

    feat(nvim): adds leetcode to nvim

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,10 @@ end
 zoxide init --cmd cd fish | source # zoxide, a better cd
 starship init fish | source # prettify the shell
 mise activate fish | source # tool manager
+
+# pnpm
+set -gx PNPM_HOME "/home/myles/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end

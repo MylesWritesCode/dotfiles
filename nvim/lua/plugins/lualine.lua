e9f5e4a2c225c05bc24c12af01d60f511549225a
Myles Berueda <myles@themapletree.io>
2025-04-06 22:10:23 -1000

    fix(nvim): fixes lualine from ai

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -79,36 +79,34 @@ local function pretty_path(opts)
 end
 
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = function(_, opts)
-      opts.options = {
-        section_separators = { left = "", right = "" },
-        component_separators = { left = "", right = "" },
-      }
-      opts.sections.lualine_a = {
-        {
-          function()
-            -- return ""
-            return ""
-          end,
-          padding = { left = 1, right = 0 },
-          separator = "",
-        },
-        "mode",
-      }
-      opts.sections.lualine_c[4] = { pretty_path() }
-      opts.sections.lualine_y = { "progress" }
-      opts.sections.lualine_z = {
-        { "location", separator = "" },
-        {
-          function()
-            return ""
-          end,
-          padding = { left = 0, right = 1 },
-        },
-      }
-      return opts
-    end,
-  },
+  "nvim-lualine/lualine.nvim",
+  opts = function(_, opts)
+    opts.options = {
+      section_separators = { left = "", right = "" },
+      component_separators = { left = "", right = "" },
+    }
+    opts.sections.lualine_a = {
+      {
+        function()
+          -- return ""
+          return ""
+        end,
+        padding = { left = 1, right = 0 },
+        separator = "",
+      },
+      "mode",
+    }
+    opts.sections.lualine_c[4] = { pretty_path() }
+    opts.sections.lualine_y = { "progress" }
+    opts.sections.lualine_z = {
+      { "location", separator = "" },
+      {
+        function()
+          return ""
+        end,
+        padding = { left = 0, right = 1 },
+      },
+    }
+    return opts
+  end,
 }
